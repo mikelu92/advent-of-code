3baad960c2f555c24426a6c5c60a642d26734246
Michael Lu <michael.lu@ad-juster.com>
2017-12-01 16:36:59 -0800

    cleaned up day 1 part 1

--- aoc1_1/main.go
+++ aoc1_1/main.go
@@ -5,43 +5,21 @@ import (
 	"io/ioutil"
 )
 
-func solution(input []byte) {
-	sum := 0
-	off := len(input) / 2 // for part 1 use 1 for off
-	for i, c := range input {
-		n := int(c - '0')
-		n2 := int(input[(i+off)%len(input)] - '0')
-		if n == n2 {
-			sum += n
-		}
-	}
-	fmt.Println(sum)
-}
-
 func main() {
-	//input := []byte{'9', '1', '2', '9'}
 	input, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		fmt.Print(err)
 	}
 
 	input = input[:len(input)-1]
-	fmt.Print(input)
-	//solution(input)
 
 	prev := int(input[len(input)-1] - '0')
 	sum := 0
 	for _, char := range input {
 		n := int(char - '0')
-		fmt.Print(prev)
-		fmt.Print(" ")
-		fmt.Print(n)
 		if prev == n {
 			sum += n
-			fmt.Print(" ")
-			fmt.Print(sum)
 		}
-		fmt.Println()
 		prev = n
 	}
 	fmt.Println(sum)
