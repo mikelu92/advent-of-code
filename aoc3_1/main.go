8d6e9554a73ee329e4b0b747ee9415eb6812748b
Michael Lu <mike.lu.92@gmail.com>
2017-12-03 11:25:22 -0800

    added better answer for aoc3_1

--- aoc3_1/main.go
+++ aoc3_1/main.go
@@ -58,21 +58,42 @@ func getCoords(target int, length int, cursor int, side int) (x int, y int) {
 }
 
 func main() {
-	input := 347991
+	input := 1024
 	// Calculate the square root
 	// find the closes odd number
 	// traverse the grid back or forward until you find the number
 	// calculate distance from center
+	result := betterAnswer(input)
 	sqrt := math.Sqrt(float64(input))
 	length := int(sqrt) + 1
 	if length%2 == 0 {
 		length++
 	}
 	side, cursor := getSide(input, length)
-	fmt.Printf("%d,%d\n", side, cursor)
 	x, y := getCoords(input, length, cursor, side)
-	fmt.Printf("%d,%d\n", x, y)
 	distance := math.Abs(float64(x-length/2)) + math.Abs(float64(y-length/2))
-	fmt.Println(distance)
+	fmt.Printf("%d,%f\n", result, distance)
 
+}
+
+func betterAnswer(input int) int {
+	cursor := 0
+	length := 0
+	for i := 3; cursor < input; i += 2 {
+		cursor = i * i
+		length = i
+	}
+	result := length - 1
+	for i := 0; cursor > input; i++ {
+		cursor -= length - 1
+	}
+	for i := 0; cursor < input; i++ {
+		if i < length/2 {
+			result--
+		} else {
+			result++
+		}
+		cursor++
+	}
+	return result
 }
